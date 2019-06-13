151f6035862a519f2b126aab818da6b5e962b193
NEKit-NG <qyb@dup2.com>
2019-06-13 14:50:08 +0800

    init proxy

--- script/diff.sh
+++ script/diff.sh
@@ -1,13 +1,19 @@
-for var in `find src -name \*.swift` ; do
+for var in `find src proxy -name \*.swift` ; do
 	filename=${var##*/}
-	src=`find ../NEKit/src -name $filename`
-	if [ $src"TEST" == "TEST" ] ; then
-		echo $filename
-		echo 
-	else
-		echo $filename
-		echo $src
-		diff $var $src
-		echo 
-	fi
+	case $filename in
+		Helper.swift) echo $filename;;
+		PipeEvent.swift) echo $filename; diff $var ../NEKit/src/Event/Event/TunnelEvent.swift;;
+		Pipe.swift) echo $filename; diff $var ../NEKit/src/Tunnel/Tunnel.swift;;
+		IPort.swift) echo $filename; diff $var ../NEKit/src/Utils/Port.swift;;
+		*) src=`find ../NEKit/src -name $filename`
+			if [ $src"TEST" == "TEST" ] ; then
+				echo ERROR: $var
+				echo 
+			else
+				echo $filename
+				echo $src
+				diff $var $src
+				echo 
+			fi
+	esac
 done
