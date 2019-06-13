dfae7eeb2ac83590d97c9177fe9c5874172d41f2
qyb <qyb@dup2.com>
2019-06-13 20:48:48 +0800

    create ProxyKit-Demo target

---
+++ ProxyKit-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProxyKit-Demo
+//
+//  Created by Dell 7460 on 2019/6/13.
+//  Copyright © 2019 NEKit-NG Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
