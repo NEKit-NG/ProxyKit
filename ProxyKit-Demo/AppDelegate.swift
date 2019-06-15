66e907510a59c3148b217c489bb3d73797fca381
NEKit-NG <51239440+NEKit-NG@users.noreply.github.com>
2019-06-15 18:27:58 +0800

    Update AppDelegate.swift

--- ProxyKit-Demo/AppDelegate.swift
+++ ProxyKit-Demo/AppDelegate.swift
@@ -47,6 +47,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        httpProxy?.stop()
+        socks5Proxy!.stop()
     }
 
 
